98441cc76ed709b207db065dd73e42b7b3c536b5
Ilya <iluhapolitov@gmail.com>
2023-05-22 20:45:30 +0300

    Task1 added and renamed to Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 2 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b){
+    Console.WriteLine("Максимум равен:" + a);
+}
+else if(b > a){
+    Console.WriteLine("Максимум равен:" + b);
+}
+else {
+    Console.WriteLine("Максимума нет");
+}
