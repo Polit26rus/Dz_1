f88675f98fcde6e9f590f960443b3bd876b19d04
Ilya <iluhapolitov@gmail.com>
2023-05-22 21:12:32 +0300

    Task4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите 3 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int ma = a;
+
+if(a == b && b == c){
+    Console.WriteLine("Максимума нет");
+}
+
+else
+    {
+        if(a >= b){
+        ma = a;
+        }
+        else {
+            ma = b;
+        }
+    if(ma > c){
+        ma = ma;
+    }
+    else if(ma < c){
+        ma = c;
+    }
+    else { ma = ma; }
+
+    Console.WriteLine("Максимум равен: " + ma);
+    
+    }
